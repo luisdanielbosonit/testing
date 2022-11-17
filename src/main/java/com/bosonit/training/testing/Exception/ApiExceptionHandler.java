4aa6bb0f85fab58d7588af4ecab44ce7835c9b76
luisdaniel.acosta <luisdaniel.acosta@bosonit.com>
2022-11-17 13:08:57 +0100

    sonarQube

--- src/main/java/com/bosonit/training/testing/Exception/ApiExceptionHandler.java
+++ src/main/java/com/bosonit/training/testing/Exception/ApiExceptionHandler.java
@@ -1,10 +1,7 @@
 package com.bosonit.training.testing.Exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice
 @ResponseBody
--- src/main/java/com/bosonit/training/testing/entity/Persona.java
+++ src/main/java/com/bosonit/training/testing/entity/Persona.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -13,6 +14,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Entity
 @Table(name= "persona")
+@RequestMapping("add")
 public class Persona  {
 
     private static final String fechtype = null;
